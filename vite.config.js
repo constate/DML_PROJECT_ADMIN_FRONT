f3ede1b7793386bc1c4fbe10baac0f1a9df312a2
sunghyun Kim <constate93@gmail.com>
2025-03-11 17:06:08 +0900

    절대경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: '@', replacement: '/src' },
+            { find: '@assets', replacement: '/src/assets' },
+            { find: '@components', replacement: '/src/components' },
+            { find: '@hooks', replacement: '/src/hooks' },
+            { find: '@pages', replacement: '/src/pages' },
+            { find: '@router', replacement: '/src/router' },
+            { find: '@styles', replacement: '/src/styles' },
+            { find: '@utils', replacement: '/src/utils' },
+        ],
+    },
+});
