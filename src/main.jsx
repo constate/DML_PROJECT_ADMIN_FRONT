9078d8ad754d2516566b2230c5b34742b2072970
sunghyun Kim <constate93@gmail.com>
2025-03-11 17:10:05 +0900

    i18n utils import

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { GlobalStyles } from './styles/GlobalStyles.js';
 import './index.css';
 import App from './App.jsx';
+import '@utils/locales/i18n';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
