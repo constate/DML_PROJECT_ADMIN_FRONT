f791dd9c0a5b2e9617adc52360dfa05aa355d5b7
sunghyun Kim <constate93@gmail.com>
2025-03-11 19:16:16 +0900

    ChannelService 적용 등

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,18 @@ import { GlobalStyles } from '@styles/GlobalStyles.js';
 import './index.css';
 import App from './App.jsx';
 import '@utils/locales/i18n';
+import ChannelService from '@utils/channelTalk/ChannelService';
+
+ChannelService.loadScript();
+const CHANNEL_SERVICE_KEY = import.meta.env.VITE_DML_CHANNEL_SERVICE_KEY || '';
+ChannelService.boot({
+    pluginKey: CHANNEL_SERVICE_KEY,
+});
+ChannelService.showChannelButton();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <GlobalStyles />
         <App />
-    </StrictMode>,
+    </StrictMode>
 );
