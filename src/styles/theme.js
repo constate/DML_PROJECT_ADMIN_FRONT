2704b177ed4cdd859dea389bed7e1db7b369cc27
sunghyun Kim <constate93@gmail.com>
2025-03-11 17:20:13 +0900

    theme.js add

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+    colors: {
+        background: '#f5f5f5',
+        text: '#000000',
+        accent: '#e87461', // 배터리 패널 오렌지색
+        panel: '#e9e9e9', // 패널 배경색
+    },
+    fontSizes: {
+        small: '0.875rem',
+        medium: '1rem',
+        large: '1.5rem',
+        xlarge: '3rem',
+        xxlarge: '5rem',
+    },
+    breakpoints: {
+        mobile: '480px',
+        tablet: '768px',
+        desktop: '1024px',
+    },
+};
