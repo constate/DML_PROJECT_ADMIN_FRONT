6580c4d52614691ba08745913e8f3f563b534941
sunghyun Kim <constate93@gmail.com>
2025-03-11 19:15:42 +0900

    ThemeProvider 적용 등

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@styles/theme.js';
 import { router } from './router/Router.jsx';
 import { RouterProvider } from 'react-router-dom';
 const App = () => {
-    return <RouterProvider router={router}></RouterProvider>;
+    return (
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router}></RouterProvider>
+        </ThemeProvider>
+    );
 };
 
 export default App;
