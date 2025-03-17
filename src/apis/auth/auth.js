a20c31a966641f16e803a2f8cfd7feac3757001f
sunghyun Kim <constate93@gmail.com>
2025-03-17 21:21:16 +0900

    login api add

--- src/apis/auth/auth.js
+++ src/apis/auth/auth.js
@@ -11,3 +11,13 @@ export const signup = async (sendJson) => {
         throw error;
     }
 };
+
+export const login = async (sendJson) => {
+    try {
+        const response = await axios.post(`${DML_SERVER}/api/login`, sendJson);
+        console.log('response', response);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
