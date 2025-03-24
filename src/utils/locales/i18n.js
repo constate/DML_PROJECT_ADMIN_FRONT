8b550adb390eda2b88fa754c9122e85c9912b278
sunghyun Kim <constate93@gmail.com>
2025-03-24 19:54:37 +0900

    i18n.js 내 google sheets load add

--- src/utils/locales/i18n.js
+++ src/utils/locales/i18n.js
@@ -1,32 +1,54 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { fetchTranslations } from './fetchTranslations';
+
+// 기본 번역 (시트 데이터 로드 전 대체)
 import englishCommonJson from '@utils/locales/translationEN.json';
 import koreanCommonJson from '@utils/locales/translationKO.json';
 import japanCommonJson from '@utils/locales/translationJP.json';
 
-i18n.use(initReactI18next).init({
-    resources: {
-        LANG_EN: {
-            translation: {
-                ...englishCommonJson,
-            },
-        },
-        LANG_KO: {
-            translation: {
-                ...koreanCommonJson,
-            },
-        },
-        LANG_JP: {
-            translation: {
-                ...japanCommonJson,
-            },
-        },
+const defaultResources = {
+    LANG_EN: {
+        translation: englishCommonJson,
     },
+    LANG_KO: {
+        translation: koreanCommonJson,
+    },
+    LANG_JP: {
+        translation: japanCommonJson,
+    },
+};
+
+// i18n 초기화 (기본 번역으로 먼저 설정)
+i18n.use(initReactI18next).init({
+    resources: defaultResources,
     lng: 'LANG_KO',
     fallbackLng: 'LANG_EN',
     interpolation: {
         escapeValue: false,
     },
 });
+
+// 구글 시트에서 번역 로드
+async function loadTranslationsFromSheet() {
+    const translations = await fetchTranslations();
+
+    if (translations) {
+        // 각 언어 번역 데이터 추가
+        Object.keys(translations).forEach((langCode) => {
+            i18n.addResourceBundle(
+                langCode,
+                'translation',
+                translations[langCode],
+                true,
+                true,
+            );
+        });
+        console.log('Translations loaded from Google Sheets');
+    }
+}
+
+// 앱이 시작될 때 번역 로드
+loadTranslationsFromSheet();
 
 export default i18n;
