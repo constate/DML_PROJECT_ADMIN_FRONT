bcf346c378c4d943906002ec850727a2d768713a
sunghyun Kim <constate93@gmail.com>
2025-03-28 14:05:33 +0900

    I18nextProvider 적용 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { GlobalStyles } from '@styles/GlobalStyles.js';
+import { I18nextProvider } from 'react-i18next';
 import './index.css';
 import App from './App.jsx';
-import '@utils/locales/i18n';
 import ChannelService from '@utils/channelTalk/ChannelService';
+import { i18n, initializeI18next } from './plugins/i18next';
+initializeI18next();
 
 ChannelService.loadScript();
 const CHANNEL_SERVICE_KEY = import.meta.env.VITE_DML_CHANNEL_SERVICE_KEY || '';
 ChannelService.boot({
     pluginKey: CHANNEL_SERVICE_KEY,
 });
-// ChannelService.showChannelButton();
 ChannelService.hideChannelButton();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <GlobalStyles />
-        <App />
+        <I18nextProvider i18n={i18n}>
+            <GlobalStyles />
+            <App />
+        </I18nextProvider>
     </StrictMode>,
 );
